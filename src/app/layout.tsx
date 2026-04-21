d11afb9a9fa4e5bbf426b53046e1e35de9438b98
Bernardo <bernardo.ferreira2@gmail.com>
2026-04-21 18:18:19 +0100

    Fix Material Icons - correct crossOrigin capitalization and add font-family !important

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
     <html lang="pt-PT" className={`dark ${manrope.variable} ${inter.variable}`}>
       <head>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,0&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-surface text-on-surface min-h-screen antialiased font-sans">
