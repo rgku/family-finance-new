2f76792cdbcd000672afeaf0e6181820caa7f451
Bernardo <bernardo.ferreira2@gmail.com>
2026-04-21 18:42:56 +0100

    Fix Material Icons on mobile - use CSS @import instead of link tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,9 +58,8 @@ export default function RootLayout({
   return (
     <html lang="pt-PT" className={`dark ${manrope.variable} ${inter.variable}`}>
       <head>
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,0&display=block" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body className="bg-surface text-on-surface min-h-screen antialiased font-sans">
         <SWRegistration />
